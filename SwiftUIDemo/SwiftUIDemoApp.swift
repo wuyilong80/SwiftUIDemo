7ef9f93a9e736f352a31dd2e4de1247bb539b65c
LawrenceWu <lawrence.wu@tpisoftware.com>
2023-01-10 17:09:21 +0800

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Lawrence on 2023/1/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

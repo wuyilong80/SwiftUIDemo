38fc903309fddebd5eea2e3b1fe9309bc153235d
LawrenceWu <lawrence.wu@tpisoftware.com>
2023-01-11 10:04:30 +0800

    learn how to use Text

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Stay hungry, Stay Foolsh.\n**This is how you bold a text**. *This is how you make text italic.* You can[click this link](https://www.appcoda.com) to go to appcoda.com").fontWeight(.bold).font(.custom("MoonDance", size: 25)).foregroundColor(.red).multilineTextAlignment(.center).lineLimit(nil).truncationMode(.middle).lineSpacing(5).shadow(color: .gray, radius: 2, x: 0, y: 5)
     }
 }
 

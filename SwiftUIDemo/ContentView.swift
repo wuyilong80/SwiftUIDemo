2efd9a9b0082e43c0a3b1dbc3dc953255d096455
LawrenceWu <lawrence.wu@tpisoftware.com>
2023-02-01 15:14:41 +0800

    learn how to use button

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -9,7 +9,80 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Stay hungry, Stay Foolsh.\n**This is how you bold a text**. *This is how you make text italic.* You can[click this link](https://www.appcoda.com) to go to appcoda.com").fontWeight(.bold).font(.custom("MoonDance", size: 25)).foregroundColor(.red).multilineTextAlignment(.center).lineLimit(nil).truncationMode(.middle).lineSpacing(5).shadow(color: .gray, radius: 2, x: 0, y: 5)
+        Button {
+            print("is pressed")
+        } label: {
+            Label {
+                
+            } icon: {
+                Image(systemName: "plus.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(.purple)
+                    .scaleEffect(2)
+            }
+        }
+        .buttonStyle(RotateButtonStyle())
+        
+//        .background(.black)
+        
+
+        
+//        VStack(spacing: 30) {
+//            Button {
+//                print("Hello World!")
+//            } label: {
+//                Text("Hello World")
+//                    .foregroundColor(.white)
+//                    .font(.title)
+//                    .padding()
+//                    .background(.purple)
+//                    .cornerRadius(40)
+//                    .padding(10)
+//                    .overlay {
+//                        RoundedRectangle(cornerRadius: 40).stroke(.purple, lineWidth: 5)
+//                    }
+//            }
+//            Button {
+//                print("delete something")
+//            } label: {
+//                Label {
+//                    Text("Delete")
+//                        .fontWeight(.semibold)
+//                        .font(.title)
+//                } icon: {
+//                    Image(systemName: "trash")
+//                        .font(.largeTitle)
+//                }
+////                .frame(minWidth: 0, maxWidth: .infinity)
+////                .padding()
+////                .background(LinearGradient(colors: [.red, .blue], startPoint: .leading, endPoint: .trailing))
+////                .foregroundColor(.white)
+////                .cornerRadius(40)
+////                .padding(.horizontal, 20)
+////                .shadow(radius: 20)
+//            }
+//            .buttonStyle(GradientBackgroundStyle())
+//        }
+    }
+}
+
+struct GradientBackgroundStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding()
+            .foregroundColor(.white)
+            .background(LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(40)
+                .padding(.horizontal, 20)
+                .scaleEffect(configuration.isPressed ? 0.8 : 1.0)
+    }
+}
+
+struct RotateButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .rotationEffect(configuration.isPressed ? .degrees(45) : .degrees(0))
     }
 }
 

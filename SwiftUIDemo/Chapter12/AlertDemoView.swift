54f6cb837f16687c8adab2a52e48285b565b6fff
lawrence.wu <lawrence.wu@gopartner.tw>
2024-07-02 18:17:40 +0800

    chapter 12 - Sheet、Alert completed

---
+++ SwiftUIDemo/Chapter12/AlertDemoView.swift
@@ -0,0 +1,46 @@
+//
+//  AlertDemoView.swift
+//  SwiftUIDemo
+//
+//  Created by Lawrence Wu 吳易隆 on 2024/7/2.
+//
+
+import SwiftUI
+
+struct AlertDemoView: View {
+    
+//    @State var showDetailView = false
+    @State var selectedArticle: Article?
+    
+    var body: some View {
+        NavigationView(content: {
+            List(articles) { article in
+                ArticleListView(article: article)
+                    .onTapGesture {
+//                        self.showDetailView = true
+                        self.selectedArticle = article
+                    }
+                .listRowSeparator(.hidden)
+            }
+            .listStyle(.plain)
+            /// .sheet use Bool control
+//            .sheet(isPresented: $showDetailView, content: {
+//                if let selectedArticle = self.selectedArticle {
+//                    ArticleDetailView(article: selectedArticle)
+//                }
+//            })
+            
+            /// .sheet use Object control
+            .sheet(item: $selectedArticle, content: { article in
+                ArticleDetailPresentView(article: article)
+            })
+            
+            .navigationTitle("Your Reading")
+        })
+        .navigationViewStyle(.stack)
+    }
+}
+
+#Preview {
+    AlertDemoView()
+}
